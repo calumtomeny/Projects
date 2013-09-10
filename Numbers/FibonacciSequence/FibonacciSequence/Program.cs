9db35e3dd46fc099fbd92f0ff1e5f3df98ff1f11
calumtomeny <calumtomeny@gmail.com>
2013-09-10 19:38:39 +0100

    Added fibonacci sequence

---
+++ Numbers/FibonacciSequence/FibonacciSequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fibonacci_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nthNumber;
+
+            while (!int.TryParse(Console.ReadLine(), out nthNumber))
+            {
+            }
+
+            Console.WriteLine(Fibonacci(nthNumber));
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static int Fibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
