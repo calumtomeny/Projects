91c2e0b878479cf3e91a371214ac479d7acec84f
calumtomeny <calumtomeny@gmail.com>
2013-09-14 00:43:18 +0100

    Added collatz conjecture

---
+++ Classic Algorithms/CollatzConjecture/CollatzConjecture/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CollatzConjecture
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int steps = 0;
+
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+
+            while (n != 1)
+            {
+                // Half Or Tripple Plus One
+                if (n % 2 == 0)
+                {
+                    n = n / 2;
+                }
+                else
+                {
+                    n = (n * 3) + 1;
+                }
+
+                steps++;
+            }
+
+            // Print the number of steps to get from n to one.
+            Console.WriteLine(steps);
+        }
+    }
+}
