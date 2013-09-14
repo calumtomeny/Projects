7dfa73bf73122bfbd44c346278ecca920fad2d4e
calumtomeny <calumtomeny@gmail.com>
2013-09-14 01:15:03 +0100

    Added reverse a string

---
+++ Text/ReverseAString/ReverseAString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string toReverse = Console.ReadLine();
+            string reversedString = null;
+
+            for (int i = toReverse.Length - 1; i >= 0; i--)
+            {
+                reversedString += toReverse[i];
+            }
+
+            Console.WriteLine(reversedString);
+        }
+    }
+}
